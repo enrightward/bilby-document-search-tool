752fbc9d96944dfba2ab14070b37212fe61f5485
Stephen Enright-Ward <steveleward@gmail.com>
2022-10-12 21:55:35 +1100

    draft results page design

--- jsxcode/src/pages/Results.jsx
+++ jsxcode/src/pages/Results.jsx
@@ -1,22 +1,17 @@
 import React from "react"
 import { useLocation } from "react-router-dom"
+import ResultCard from "../components/ResultCard/ResultCard.jsx"
 
 export default function Results() {
     const location = useLocation()
+    const searchTerm = location.state.searchTerm
 
     return (
-        <div>
-            <h1>Search Results</h1>
-            <p>
-                This page will display search results.
-            </p>
-            <p>
-                THE STATE IS: {location.state.searchTerm}
-            </p>
-            <p>
-                Bilby AI is a project of{" "}
-                <a href="https://www.bilby.ai">Bilby AI Pty Ltd</a>.
-            </p>
-        </div>
+        <>
+            <ResultCard searchTerm={searchTerm} />
+            <ResultCard searchTerm={searchTerm} />
+            <ResultCard searchTerm={searchTerm} />
+            <ResultCard searchTerm={searchTerm} />
+        </>
     )
 }
